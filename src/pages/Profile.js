fbf02c31449fb830f9108fefba193436d84686fe
hamsame <hamsaaelmi@gmail.com>
2023-01-02 22:32:26 +0000

    Added signout functionality

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,25 @@
 import React from "react"
 import { Box, Container } from "@mui/system"
-import { getAuth } from "firebase/auth"
+import { getAuth, signOut } from "firebase/auth"
+import { toast } from "react-toastify"
+import { useNavigate } from "react-router-dom"
 
 function Profile() {
   const auth = getAuth()
+  const navigate = useNavigate()
+  const signOutOfApp = () => {
+    try {
+      signOut(auth)
+      navigate("/signin")
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <Container sx={{ width: "90%", minHeight: "90vh", margin: "auto" }}>
       <h1>Gloat</h1>
       <h2>{auth.currentUser.displayName || "Profile"}</h2>
+      <button onClick={signOutOfApp}>Logout</button>
     </Container>
   )
 }
