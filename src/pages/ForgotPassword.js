dfb87c240794dc5f3d60eb28c8bd782c51df5ec9
hamsame <hamsaaelmi@gmail.com>
2023-01-02 22:22:01 +0000

    Added password reset functionality

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -6,15 +6,27 @@ import { Box, Container } from "@mui/system"
 import { FormControl, Input, InputLabel } from "@mui/material"
 import Button from "@mui/material/Button"
 
-function ForgotPassword() {
-  const [showPassword, setShowPassword] = useState(false)
-  const [email, setEmail] = useState("")
+import { getAuth, sendPasswordResetEmail } from "firebase/auth"
+import { toast } from "react-toastify"
 
+function ForgotPassword() {
+  const [email, setEmail] = useState("")
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+      await sendPasswordResetEmail(auth, email)
+      toast.success("Password Reset Email Sent!")
+      setEmail("")
+    } catch (error) {
+      toast.error("Enter your email")
+    }
+  }
   return (
     <>
       <Container sx={{ width: "90%", minHeight: "90vh", margin: "auto" }}>
         <h1>Gloat!</h1>
-        <Box component="form" autoComplete="off">
+        <Box component="form" autoComplete="off" onSubmit={handleSubmit}>
           <h3>Reset Your Password</h3>
           <FormControl sx={formStyle} variant="standard">
             <InputLabel htmlFor="email">Email Address: </InputLabel>
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,15 @@
 import React from "react"
+import { Box, Container } from "@mui/system"
+import { getAuth } from "firebase/auth"
 
 function Profile() {
-  return <div>Profile</div>
+  const auth = getAuth()
+  return (
+    <Container sx={{ width: "90%", minHeight: "90vh", margin: "auto" }}>
+      <h1>Gloat</h1>
+      <h2>{auth.currentUser.displayName || "Profile"}</h2>
+    </Container>
+  )
 }
 
 export default Profile
