46b31392347db81f0639b944950133c0aaac9538
hamsame <hamsaaelmi@gmail.com>
2023-01-02 21:21:01 +0000

    Added signin functionality

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { formStyle, inputStyle, buttonStyle } from "../components/styles"
 
 import { Box, Container } from "@mui/system"
@@ -9,6 +9,9 @@ import Visibility from "@mui/icons-material/Visibility"
 import VisibilityOff from "@mui/icons-material/VisibilityOff"
 import InputAdornment from "@mui/material/InputAdornment"
 import Button from "@mui/material/Button"
+
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
+import { toast } from "react-toastify"
 
 function Signin() {
   const [showPassword, setShowPassword] = useState(false)
@@ -22,11 +25,31 @@ function Signin() {
     setFormData({ ...formData, [e.target.name]: e.target.value })
   }
 
+  const navigate = useNavigate()
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
+      const user = userCredential.user
+      console.log(user.uid)
+      setTimeout(() => {
+        navigate("/profile")
+      }, 1000)
+    } catch (error) {
+      toast.error("Incorrect Email and Password Combination")
+    }
+  }
+
   return (
     <>
       <Container sx={{ width: "90%", minHeight: "90vh", margin: "auto" }}>
         <h1>Gloat!</h1>
-        <Box component="form" autoComplete="off">
+        <Box component="form" autoComplete="off" onSubmit={handleSubmit}>
           <h3>Sign In</h3>
           <FormControl sx={formStyle} variant="standard">
             <InputLabel htmlFor="email">Email Address: </InputLabel>
@@ -60,7 +83,7 @@ function Signin() {
               }
             />
           </FormControl>
-          <Button sx={buttonStyle} type="submit">
+          <Button sx={buttonStyle} type="submit" onClick={() => handleSubmit()}>
             Sign In
           </Button>
           <p>
