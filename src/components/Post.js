e03b8c93bdf6bf89269b4216b6bbee88917a55ba
hamsame <hamsaaelmi@gmail.com>
2023-02-19 14:04:36 +0000

    seemed to have solved some error

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,69 +1,14 @@
 import { Box } from "@mui/system"
-import React, { useState } from "react"
+import React from "react"
 import { getAuth } from "firebase/auth"
-import { doc, deleteDoc } from "firebase/firestore"
 
-const Post = ({
-  index,
-  caption,
-  username,
-  imgURLS,
-  profilePic,
-  userRef,
-  date,
-}) => {
-  const auth = getAuth()
-  const userID = auth.currentUser.uid
-
-  // add edit btn/ delete btn
-  const deleteDoc = () => {}
-
+const Post = ({ caption, username, imgURLS, profilePic, userRef, date }) => {
   return (
-    <Box key={index} sx={{ my: 4, background: " " }}>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "flex-start",
-          alignItems: "center",
-          width: "100%",
-          gap: "5rem",
-          margin: "1rem 0",
-        }}
-        dataUserId={userRef}
-      >
-        <img
-          src={
-            profilePic ||
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR82DN9JU-hbIhhkPR-AX8KiYzA4fBMVwjLAG82fz7GLg&s"
-          }
-          style={{ borderRadius: "50%" }}
-          alt="Profile avatar"
-        />
-        <p className="capitalise username">{username}</p>
-      </div>
-      <p>{caption}</p>
-      {imgURLS?.map((img, index) => {
-        return (
-          <Box
-            component="img"
-            sx={{
-              height: 1000,
-              width: 1000,
-              maxHeight: { xs: 233, md: 167 },
-              maxWidth: { xs: 350, md: 250 },
-              mr: "2rem",
-              my: 2,
-            }}
-            key={index}
-            src={img}
-          />
-        )
-      })}
-      {userRef === userID ? (
-        <button onClick={() => deleteDoc()}>delete post</button>
-      ) : (
-        ""
-      )}
+    <Box sx={{ my: 4 }}>
+      <h1>{caption}</h1>
+      <h2>
+        {username},{date}
+      </h2>
     </Box>
   )
 }
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,22 +1,67 @@
 import React from "react"
 import Post from "./Post.js"
 import { Box } from "@mui/system"
+import { getAuth } from "firebase/auth"
 
 const Posts = ({ posts }) => {
+  const auth = getAuth()
+  const deleteDoc = () => {
+    try {
+      console.log("delete doc")
+    } catch (error) {
+      console.log("error")
+    }
+  }
   return (
     <Box>
       {posts.map((post, index) => {
         const { caption, userRef, date, username, imgURLS } = post
+        let profilePic
+
         return (
-          <Post
-            key={index}
-            index={index}
-            caption={caption}
-            username={username}
-            imgURLS={imgURLS}
-            userRef={userRef}
-            date={date}
-          />
+          <Box key={index} sx={{ my: 4, background: " " }}>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "flex-start",
+                alignItems: "center",
+                width: "100%",
+                gap: "5rem",
+                margin: "1rem 0",
+              }}
+            >
+              <img
+                src={
+                  profilePic ||
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR82DN9JU-hbIhhkPR-AX8KiYzA4fBMVwjLAG82fz7GLg&s"
+                }
+                style={{ borderRadius: "50%" }}
+                alt="Profile avatar"
+              />
+              <p className="capitalise username">{username}</p>
+            </div>
+            <p>{caption}</p>
+            {imgURLS?.map((img, el) => {
+              return (
+                <Box
+                  component="img"
+                  sx={{
+                    height: 1000,
+                    width: 1000,
+                    maxHeight: { xs: 233, md: 167 },
+                    maxWidth: { xs: 350, md: 250 },
+                    mr: "2rem",
+                    my: 2,
+                  }}
+                  key={el}
+                  src={img}
+                />
+              )
+            })}
+            {userRef === auth.currentUser.uid && (
+              <button onClick={() => deleteDoc()}>delete post</button>
+            )}
+          </Box>
         )
       })}
     </Box>
